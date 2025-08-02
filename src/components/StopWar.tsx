f01e9e5be725222eef8568be8b687b4d7aec2829
araguaci <araguaci@gmail.com>
2025-08-02 19:25:47 -0300

    refactor: reorganizar estrutura do componente StopWar para melhor legibilidade

--- src/components/StopWar.tsx
+++ src/components/StopWar.tsx
@@ -1,14 +1,34 @@
 export const StopWar = () => {
   return (
-    <div className="p-4 text-center flex items-center justify-center">
-      <a
-        href="https://stop-war-for-ever.vercel.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-      >
-        <img src="https://img.shields.io/badge/STOP%20WAR%20FOR%20EVER-6d3d9d?logo=alchemy&logoColor=00aced&style=flat"/>
-      </a>
+    <div className="p-4 text-center items-center justify-center">
+      <hr/>
+      <div className="p-4 text-center flex items-center justify-center">
+        <a href="https://stop-war-for-ever.vercel.app" className='display-inline-block m-2'>
+          <img
+            src="https://img.shields.io/badge/STOP%20WAR%20FOR%20EVER-6d3d9d?logo=alchemy&logoColor=00aced&style=flat"
+            alt="STOP-WAR-FOR-EVER"
+          />
+        </a>
+        <a href="https://abusosupremo.vercel.app" className='display-inline-block m-2'>
+          <img
+            src="https://img.shields.io/badge/ABUSO%20SUPREMO-351d1d?logo=msi&logoColor=6bc175&style=flat"
+            alt="ABUSO SUPREMO"
+          />
+        </a>
+        <a href="https://futuroroubado.vercel.app" className='display-inline-block m-2'>
+          <img
+            src="https://img.shields.io/badge/FUTURO%20ROUBADO-417547?logo=redragon&logoColor=efefef&style=flat"
+            alt="FUTURO ROUBADO"
+          />
+        </a>
+        <a href="https://abusofederal.vercel.app" className='display-inline-block m-2'>
+          <img
+            src="https://img.shields.io/badge/ABUSO%20FEDERAL-00295b?logo=diagramsdotnet&logoColor=bf2b2b&style=flat"
+            alt="ABUSO FEDERAL"
+          />
+        </a>
+      </div>        
+      <h3>✠ Projeto Voluntário ✠ © 2025</h3>
     </div>
   );
 };
